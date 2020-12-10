9d15bd2658a0438f3b600c5f639d347e0ba5025f
Sean Harrison <sah@bookgenesis.com>
2020-12-09 18:57:49 -0600

    settings bugfix

--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,7 @@ TESTING = bool(os.getenv('TESTING'))
 DATABASE_URL = os.environ['DATABASE_URL']
 DATABASE_PARAMS = dict(
     # force transaction rollback if we're pytesting the app
-    force_rollback=settings.TESTING,
+    force_rollback=TESTING,
     # connection pool options
     min_size=os.getenv('DATABASE_POOL_MIN_SIZE', 5),
     max_size=os.getenv('DATABASE_POOL_MAX_SIZE', 5),
