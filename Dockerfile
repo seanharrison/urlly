7da9072c96a8148dffea2eef220680b32548d468
Sean Harrison <sah@kruxia.com>
2022-10-03 23:15:07 -0500

    update system to Debian bullseye, python 10, python requirements. pin production requirements.txt exactly, move semantic requirements into setup.py, move setup.json into setup.py, separate dev and test requirements

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM python:3.8-slim-buster
+FROM python:3.10-slim-bullseye AS build
 LABEL maintainer="Sean Harrison <sah@kruxia.com>"
 
 RUN apt-get update \
     && apt-get install -y \
-        postgresql-client \
+    postgresql-client \
     && apt-get upgrade -y \
     && rm -rf /var/lib/apt/lists/*
 
@@ -11,8 +11,15 @@ WORKDIR /app
 COPY ./requirements.txt /app/requirements.txt
 RUN pip install -r /app/requirements.txt
 COPY ./ /app
-RUN pip install -e .
 
+FROM build AS deploy
 EXPOSE 8000
 ENTRYPOINT [ "./docker-entrypoint.sh" ]
+CMD ["gunicorn", "-w", "4", "-k", "uvicorn.workers.UvicornWorker", "--bind", "0.0.0.0:8000", "--log-level", "debug", "app.main:app"]
+
+FROM deploy AS test
+RUN pip install -e .[test]
+
+FROM deploy AS develop
+RUN pip install -e .[dev,test]
 CMD ["uvicorn", "app.main:app", "--reload", "--host", "0.0.0.0", "--port", "8000", "--log-level", "debug"]
--- setup.py
+++ setup.py
@@ -4,16 +4,61 @@ from codecs import open
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(PATH, "setup.json"), encoding='utf-8') as f:
-    CONFIG = json.load(f)
-
 with open(os.path.join(PATH, 'README.md'), encoding='utf-8') as f:
     README = f.read()
-    
+
 if __name__ == '__main__':
     setup(
+        name="urlly",
+        version="0.1.0-alpha",
+        description="Shorten urls",
         long_description=README,
+        long_description_content_type='text/markdown',
+        url="https://github.com/seanharrison/urlly",
+        author="Sean Harrison",
+        author_email="sah@bookgenesis.com",
+        license="MPL 2.0",
+        classifiers=[
+            "Development Status :: 1 - Planning",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+            "Programming Language :: Python :: 3",
+        ],
+        install_requires=[
+            "aiofiles~=22.1.0",
+            "alembic~=1.8.1",
+            "asyncpg~=0.26.0",
+            "databases~=0.4.3",
+            "gunicorn~=20.1.0",
+            "httptools~=0.5.0",
+            "Jinja2~=2.11.3",
+            "MarkupSafe~=2.0.1",
+            "orjson~=3.8.0",
+            "psycopg2-binary~=2.9.3",
+            "pydantic~=1.10.2",
+            "requests~=2.28.1",
+            "SQLAlchemy~=1.3.24",
+            "starlette~=0.21.0",
+            "uvicorn~=0.18.3",
+            "uvloop~=0.17.0",
+        ],
+        extras_require={
+            "dev": [
+                "black~=22.8.0",
+                "ipython~=8.5.0",
+                "isort~=5.10.1",
+            ],
+            "test": [
+                "black~=22.8.0",
+                "flake8~=5.0.4",
+                "httpx~=0.23.0",
+                "pytest~=7.1.3",
+                "pytest-cov~=4.0.0",
+            ],
+        },
+        entry_points={},
         packages=find_packages(),
-        install_requires=[],
-        **CONFIG
+        package_data={"": []},
+        data_files=[],
+        scripts=[],
     )
